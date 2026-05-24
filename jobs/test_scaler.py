ac0b2b5c072d0b47d2add1751b3835e3aaef9f4d
allen <ylchen1805@gmail.com>
2026-05-24 19:07:21 +0800

    Add parameter to test_scaluer.py, easier for later test

--- jobs/test_scaler.py
+++ jobs/test_scaler.py
@@ -1,5 +1,10 @@
 # jobs/test_scaler.py
 from autoscaler.scaler import scale_down, scale_up
+import sys
 
-scale_up(1)
-# scale_down(1)
+if len(sys.argv) < 2:
+    scale_up(1)
+elif sys.argv[1] == "up":
+    scale_up(1)
+elif sys.argv[1] == "down":
+    scale_down(1)
